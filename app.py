d1aaf383fc9ab76debad26aa326172a702fb9a83
sindhuragajjala <121331979+sindhuragajjala@users.noreply.github.com>
2024-08-26 18:55:08 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,71 @@
+import streamlit as st
+import json
+import requests
+import base64
+from PIL import Image
+import io
+
+
+#these are main classes your image is trained on
+#you can define the classes in alphabectical order
+PREDICTED_LABELS = ["benign","malignant","normal"]  # edit 1
+PREDICTED_LABELS.sort()
+
+
+def get_prediction(image_data):
+  #replace your image classification ai service URL
+  url = 'https://askai.aiclub.world/b0fb53fa-e1cc-4e66-aa95-e86d7abf8ea7'  #Edit 2
+  r = requests.post(url, data=image_data)
+  response = r.json()['predicted_label']
+  score = r.json()['score']
+  #print("Predicted_label: {} and confidence_score: {}".format(response,score))
+  return response, score
+
+
+#creating the web app
+
+#setting up the title
+st.title("Breast Ultra Sound Image Classifier")#change according to your project   #edit 3
+#setting up the subheader
+st.subheader("BUSI")#change according to your project
+
+#file uploader
+image = st.file_uploader(label="Upload an image",accept_multiple_files=False, help="Upload an image to classify them")
+if image:
+    #converting the image to bytes
+    img = Image.open(image)
+    buf = io.BytesIO()
+    img.save(buf,format = 'JPEG')
+    byte_im = buf.getvalue()
+
+    #converting bytes to b64encoding
+    payload = base64.b64encode(byte_im)
+
+    #file details
+    file_details = {
+      "file name": image.name,
+      "file type": image.type,
+      "file size": image.size
+    }
+
+    #write file details
+    st.write(file_details)
+
+    #setting up the image
+    st.image(img)
+
+    #predictions
+    response, scores = get_prediction(payload)
+
+    #if you are using the model deployment in navigator
+    #you need to define the labels
+    response_label = PREDICTED_LABELS[response]
+
+    col1, col2 = st.columns(2)
+    with col1:
+      st.metric("Prediction Label",response_label)
+    with col2:
+      st.metric("Confidence Score", max(scores))
+
+
+
